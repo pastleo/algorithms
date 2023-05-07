474e092c43eb64e28624c085c44c0d6bdcc362ad
PastLeo <chgu82837@gmail.com>
2023-05-08 00:24:59 +0800

    add elixir Algorithms.missing_number generated by autoGPT

--- lib/algorithms.ex
+++ lib/algorithms.ex
@@ -1,4 +1,8 @@
 defmodule Algorithms do
+  def missing_number(arr) do
+    Enum.sum(1..(length(arr))) - Enum.sum(arr)
+  end
+
   def plus(a, b) do
     a + b
   end
--- test/algorithms_test.exs
+++ test/algorithms_test.exs
@@ -1,8 +1,19 @@
 defmodule AlgorithmsTest do
   use ExUnit.Case
-  import Algorithms
 
-  test "1 + 1 = 2" do
-    assert plus(1, 1) == 2
+  describe "missing_number/1" do
+    test "returns the missing number in an array" do
+      assert Algorithms.missing_number([0, 1, 2, 3, 5]) == 4
+    end
+
+    test "returns the missing number in an array, which is last number" do
+      assert Algorithms.missing_number([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]) == 21
+    end
+  end
+
+  describe "plus/2" do
+    test "adds two numbers together" do
+      assert Algorithms.plus(2, 3) == 5
+    end
   end
 end
